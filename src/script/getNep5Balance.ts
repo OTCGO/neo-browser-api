6303f0de5027bd3c0e11d730b7c73554ca197c91
503945930 <503945930@qq.com>
2018-04-15 22:21:51 +0800

    fix: nep5 balance

--- src/script/getNep5Balance.ts
+++ src/script/getNep5Balance.ts
@@ -8,7 +8,7 @@ import { Decimal } from 'decimal.js'
 import { Request as WebHandler } from '../utils'
 import { api } from '@cityofzion/neon-js'
 import { parallel } from '../utils/index'
-import { DBClient , client as redis } from '../lib'
+import { DBClient, client as redis } from '../lib'
 
 
 
@@ -22,28 +22,26 @@ const dbUtxolClient: any = new DBClient(config.get('dbUtxo'))
 // score  balance
 // value  balance
 
-const async = require('async')
-
-const q = async.queue(function(data, callback) {
-// fork callback
-  callback()
-}, 5)
 
 
 async function main() {
 
   const dbGlobal = await dbGlobalClient.connection()
-  const cursor = await dbGlobal.address.find().sort({blockIndex: 1})
+  const cursor = await dbGlobal.address.find().sort({ blockIndex: 1 })
 
+  // cursor.forEach(async data => {
+  //   await getBalance(data.address)
+  //   console.log('data')
+  // })
   cursor.on('data', async (data) => {
     try {
-      await getBalance(data.address)
+      getBalance(data.address)
+
+      console.log('data')
+
     } catch (error) {
       console.log('error', error)
     }
-
-    // q.push(await getBalance(data.address))
-
   })
   cursor.on('end', () => {
     console.log('end')
@@ -55,47 +53,56 @@ async function main() {
 }
 
 
-async function getBalance(address) {
-
+function getBalance(address) {
+  console.log('getBalance', address)
+  return new Promise(async (resolve, reject) => {
     try {
-    const dbGlobal = await dbGlobalClient.connection()
+      const dbGlobal = await dbGlobalClient.connection()
+      const asset: any = await dbGlobal.asset.find({ type: 'nep5', status: { $exists: false } }).toArray()
+      const arr = []
 
-
-    const asset: any = await dbGlobal.asset.find({type: 'nep5', status: {$exists: false}}).toArray()
-    const arr = []
-
-    asset.forEach(item => {
-      arr.push(async () => {
-        const balances = await api.nep5.getTokenBalance(config.get('rpc'), item.assetId.substring(2), address)
-        return {
-          assetId: item.assetId,
-          name: item.symbol,
-          type: 'nep5',
-          balances: new Decimal(`${balances || 0}`)
-        }
+      asset.forEach(item => {
+        arr.push(async () => {
+          const balances = await api.nep5.getTokenBalance(await getNode(), item.assetId.substring(2), address)
+          return {
+            assetId: item.assetId,
+            name: item.symbol,
+            type: 'nep5',
+            balances: new Decimal(`${balances || 0}`)
+          }
+        })
       })
-  })
-  const result: any = await parallel(arr, 10)
-  result.forEach((item) => {
-         // console.log('balances', item)
+      const result: any = await parallel(arr, 5)
+      result.forEach((item) => {
+        // console.log('balances', item)
         if (item.balances.gt(0)) {
-            // console.log('balances', item.balances)
-            redis.zadd(`${item.assetId.substring(2)}`, item.balances, JSON.stringify({
-                address,
-                balances: item.balances
-            }))
+          // console.log('balances', item.balances)
+          redis.zadd(`${item.assetId.substring(2)}`, item.balances, JSON.stringify({
+            address,
+            balances: item.balances
+          }))
         }
 
-    })
+      })
 
-    console.log('end', address)
+      console.log('end', address)
+      return resolve()
     } catch (error) {
-        console.log('error', error)
+      console.log('error', error)
+      return reject()
     }
+  })
+}
+
+async function getNode() {
+  const arr: any[] = config.get('rpclist') || []
+  const node = arr[Math.floor(Math.random() * arr.length)]
+  // console.log('node', node)
+  return node
 }
 
 // AUkVH4k8gPowAEpvQVAmNEkriX96CrKzk9
 // getBalance('AUkVH4k8gPowAEpvQVAmNEkriX96CrKzk9')
- main()
+main()
 
 // ZREVRANGE c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b 0 19
