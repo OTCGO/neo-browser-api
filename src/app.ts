4f7b6e6e7a9ce01fe9d115348a0d5693b06e5420
503945930 <503945930@qq.com>
2019-07-26 13:20:00 +0800

    add route limit

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ import * as log4js from 'log4js'
 // import { RPX } from './boot'
 import router from './router'
 import { APIOutputMiddleware } from './middlewares'
-
+import * as rateLimit from 'express-rate-limit'
 
 
 
@@ -43,6 +43,17 @@ app.use(methodOverride())
 app.use(APIOutputMiddleware)
 
 app.use(config.get('app.apiPrefix'), router)
+
+
+const limiter = rateLimit({
+  windowMs:  60 * 1000, // 1 minutes
+  max: 50, // limit each IP to 50 requests per windowMs,
+  handler:(req,res)=>{
+    return res.apiError('Too many requests, please try again later')
+  }
+});
+//  apply to all requests
+app.use(limiter);
 
 // root /
 app.get('/', function(req, res) {
